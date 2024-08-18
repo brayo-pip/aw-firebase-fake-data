95d03e9484d6a2b28e2ba5e0583c23f2ecc92e09
Brayo <vukubrian@gmail.com>
2024-08-18 11:40:28 +0300

    feat: create ScreenTimeSummaryRanked

--- functions/src/types.ts
+++ functions/src/types.ts
@@ -27,6 +27,11 @@ export interface ScreenTimeSummary {
   categoryTotals: { [key: string]: number };
 }
 
+// Same as above but with an additional ranked field
+export interface ScreenTimeSummaryRanked extends ScreenTimeSummary {
+  rank: number;
+}
+
 export interface ChartDataset {
   label: string;
   data: number[];
